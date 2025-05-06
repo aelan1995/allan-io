e89720bf2f02b1b517d9809e7dd524b45266db6d
Allan Punzalan <allanboiebiaspunzalan@gmail.com>
2025-05-06 20:12:44 +0800

    SEO Optimization: Add robots.txt and sitemap.xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,14 @@
-// next-sitemap.config.js
 module.exports = {
   siteUrl: "https://allan-io.vercel.app",
   generateRobotsTxt: true,
+  sitemapSize: 1,
+  exclude: [], // make sure no page is excluded
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: "monthly",
+      priority: 1.0,
+      lastmod: new Date().toISOString(),
+    };
+  },
 };
